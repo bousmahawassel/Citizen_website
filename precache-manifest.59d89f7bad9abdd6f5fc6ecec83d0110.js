14e9380fe9fc1d7cf1ae347987ac8ff62048b250
wasselbousmaha <wassel2005@gmail.com>
2021-06-25 22:13:58 +0200

    First build of web app

---
+++ precache-manifest.59d89f7bad9abdd6f5fc6ecec83d0110.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c75ce502d481ffeb5b1",
+    "url": "/static/js/app.b6c0e21b.chunk.js"
+  },
+  {
+    "revision": "fcd2ca09e1d83038dbb1",
+    "url": "/static/js/runtime~app.8d6b8e22.js"
+  },
+  {
+    "revision": "a625a95ce09765619565",
+    "url": "/static/js/2.14d44f6f.chunk.js"
+  },
+  {
+    "revision": "8ef52a15e44481b41e7db3c7eaf9bb83",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b3263095df30cb7db78c613e73f9499a",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "e766963327e0a89f9ec2ba88646b6177",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/static/media/star.004c2bbb.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/static/media/heart.2327736b.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/static/media/rocket.51671417.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/static/media/bell.a7b9dc9d.png"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "cd42492cc4a5ece6f7717c9f1b2612ac",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "ee0e788f858be6a386bb4014e923844f",
+    "url": "/apple/icons/icon_180x180.png"
+  },
+  {
+    "revision": "3aa0b091ff6ee2e1b6cfaad13807d702",
+    "url": "/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "16a4ec94e961dd6f40641be29ec2effd",
+    "url": "/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "7272d81606f003905eb45f40ace31668",
+    "url": "/apple/splash/icon_1125x2436.png"
+  },
+  {
+    "revision": "561a039c1a65105ff9db98da5eb290d3",
+    "url": "/apple/splash/icon_1136x640.png"
+  },
+  {
+    "revision": "9a24887d9d931a4a2de9c410fb8b0cf9",
+    "url": "/apple/splash/icon_1242x2208.png"
+  },
+  {
+    "revision": "269c33247f806746866342659374a852",
+    "url": "/apple/splash/icon_1242x2688.png"
+  },
+  {
+    "revision": "f6804dd26e5b5e786fe57ec5fbfc5df0",
+    "url": "/apple/splash/icon_1334x750.png"
+  },
+  {
+    "revision": "b48c0bc3007e8e7e60e8b2b93c97ab4e",
+    "url": "/apple/splash/icon_1792x828.png"
+  },
+  {
+    "revision": "06d33337afd9ecc6d589b56df1973659",
+    "url": "/apple/splash/icon_2208x1242.png"
+  },
+  {
+    "revision": "6b48a6cb236fe6ccfbb3393bf9ef7f3b",
+    "url": "/apple/splash/icon_2436x1125.png"
+  },
+  {
+    "revision": "a86989f643baeacb3129de99bb5c0578",
+    "url": "/apple/splash/icon_2688x1242.png"
+  },
+  {
+    "revision": "53e4bdf6afad3c3daab904bc3fba78fe",
+    "url": "/apple/splash/icon_640x1136.png"
+  },
+  {
+    "revision": "46abef2b5e0273583319d15f0b4bee61",
+    "url": "/apple/splash/icon_750x1334.png"
+  },
+  {
+    "revision": "e39c786ba4d7965b412ae94758a42827",
+    "url": "/apple/splash/icon_828x1792.png"
+  },
+  {
+    "revision": "ced48dd8483102c43619aa6fa2815232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e842c2bceccc5adf3ad04c2538875b4e",
+    "url": "/static/js/app.b6c0e21b.chunk.js.gz"
+  },
+  {
+    "revision": "a930d309750c023bfc073b7bb7b8dfbf",
+    "url": "/static/js/runtime~app.8d6b8e22.js.gz"
+  },
+  {
+    "revision": "d5c897f0f97f669b9e417ea3d269865f",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "0dba2befd65b5bdc3dc5f6e58fd0a4bc",
+    "url": "/static/js/2.14d44f6f.chunk.js.gz"
+  }
+];
